ff7e4e9bbe2bffef89edda94d2c99775c3848d78
Svein-Tore <svenar89@gmail.com>
2026-02-02 21:14:44 +0100

    Oppdater main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,20 +1,16 @@
 input.onButtonPressed(Button.A, function () {
-    sendtekst = "Pil v"
+    radio.sendString("Pil V")
 })
 input.onGesture(Gesture.Shake, function () {
-    sendtekst = "Stop"
+    radio.sendString("Stopp")
 })
 input.onButtonPressed(Button.AB, function () {
-    sendtekst = "Annet"
+    radio.sendString("Annet")
 })
 input.onButtonPressed(Button.B, function () {
-    sendtekst = "Pil H"
+    radio.sendString("Pil H")
 })
-let sendtekst = ""
 radio.setGroup(1)
 radio.setTransmitPower(7)
-sendtekst = "Stop"
+let sendtekst = "Stopp"
 basic.showIcon(IconNames.Happy)
-basic.forever(function () {
-    radio.sendString(sendtekst)
-})
